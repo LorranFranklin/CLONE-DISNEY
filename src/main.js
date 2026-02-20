31aac35eb9901d625ba3ec30144984f01075a650
Lorran <lorranfranklin.dev@gmail.com>
2026-02-20 09:51:02 -0300

    faq concluido, estilizado e feito manipulações usando js e scss para deixar o elemento quentions dinamico.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const buttons = document.querySelectorAll('[data-tab-button]');
-  // const tabsContainer = document.querySelectorAll('[data-tabs-id]');
+  const questions = document.querySelectorAll('[data-faq-question]');
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function (botao) {
@@ -12,7 +12,16 @@ document.addEventListener('DOMContentLoaded', function () {
       aba.classList.add('shows__list--is-active');
     });
   }
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener('click', abreOuFechaResposta);
+  }
 });
+
+function abreOuFechaResposta(elemento) {
+  const classe = 'faq__questions__item--is-open';
+  const elementoPai = elemento.target.parentNode;
+  elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
   const buttons = document.querySelectorAll('[data-tab-button]');
